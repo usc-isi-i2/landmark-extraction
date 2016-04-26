a5f49d94b87b73ae67a14909f379d5befe09aa13
Brian Amanatullah <bamanatullah@inferlink.com>
2016-04-26 14:28:14 -0400

    Fix PageClusterer to actually use flag to copy to webapp or not.

--- src/learning/PageClusterer.py
+++ src/learning/PageClusterer.py
@@ -14,10 +14,11 @@ class Usage(Exception):
 def cluster(project_name, working_dir_str, copy_to_webapp=False):
     
     #try to get the right directory to get the landmark online tools folder
-    working = os.getcwd()
-    while not working.endswith('/src'):
-        (working,other) = os.path.split(working)
-    web_app_projects_dir = os.path.join(working, 'angular_flask/static/project_folders')
+    if copy_to_webapp:
+        working = os.getcwd()
+        while not working.endswith('/src'):
+            (working,other) = os.path.split(working)
+        web_app_projects_dir = os.path.join(working, 'angular_flask/static/project_folders')
         
     tf = TruffleShuffle(working_dir_str)
     clusters = tf.do_truffle_shuffle(algorithm='rule_size')
@@ -27,35 +28,41 @@ def cluster(project_name, working_dir_str, copy_to_webapp=False):
         cluster_str = 'cluster' + format(clusterCount, '03')
         
         #copy it into the local angular_flask web directory
-        blank = os.path.join(web_app_projects_dir, '_blank')
-        project_dir = os.path.join(web_app_projects_dir, project_name+"_"+cluster_str)
-        shutil.copytree(blank, project_dir)
+        markup_file = None
+        if copy_to_webapp:
+            blank = os.path.join(web_app_projects_dir, '_blank')
+            project_dir = os.path.join(web_app_projects_dir, project_name+"_"+cluster_str)
+            shutil.copytree(blank, project_dir)
         
-        markup_file = os.path.join(project_dir, 'learning', 'markup.json')
-        with codecs.open(markup_file, "r", "utf-8") as myfile:
-            json_str = myfile.read().encode('utf-8')
-        markup = json.loads(json_str)
+            markup_file = os.path.join(project_dir, 'learning', 'markup.json')
+            with codecs.open(markup_file, "r", "utf-8") as myfile:
+                json_str = myfile.read().encode('utf-8')
+            markup = json.loads(json_str)
 
-        cluster_dir_str = os.path.join(clusters_dir_str, cluster_str)
-        if not os.path.exists(cluster_dir_str):
-            os.makedirs(cluster_dir_str)
         clusterCount += 1
                 
         page_count = 0;
         if len(clusters[rule]['MEMBERS']) > 5:
+            cluster_dir_str = os.path.join(clusters_dir_str, cluster_str)
+            if not os.path.exists(cluster_dir_str):
+                os.makedirs(cluster_dir_str)
+                
             for page_id in clusters[rule]['MEMBERS']:
                 copyfile(os.path.join(working_dir_str, page_id), os.path.join(cluster_dir_str, page_id))
                 
-                if page_count < 7:
-                    #and copy it to the web_app_dir if we have less than 5 there
-                    copyfile(os.path.join(working_dir_str, page_id), os.path.join(project_dir, page_id))
-                    markup['__URLS__'][page_id] = page_id
-                    markup[page_id] = {}
+                if copy_to_webapp:
+                    if page_count < 7:
+                        #and copy it to the web_app_dir if we have less than 5 there
+                        copyfile(os.path.join(working_dir_str, page_id), os.path.join(project_dir, page_id))
+                        markup['__URLS__'][page_id] = page_id
+                        markup[page_id] = {}
+
                 page_count += 1
         
-        with codecs.open(markup_file, "w", "utf-8") as myfile:
-            myfile.write(json.dumps(markup, sort_keys=True, indent=2, separators=(',', ': ')))
-            myfile.close()
+        if copy_to_webapp:
+            with codecs.open(markup_file, "w", "utf-8") as myfile:
+                myfile.write(json.dumps(markup, sort_keys=True, indent=2, separators=(',', ': ')))
+                myfile.close()
 
 def main(argv=None):
     if argv is None:
