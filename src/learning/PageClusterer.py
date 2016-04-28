00db7f272ad2dfb24158b9babe362767d7a6347d
Brian Amanatullah <bamanatullah@inferlink.com>
2016-04-28 11:13:40 -0400

    Minor PageClusterer update.

--- src/learning/PageClusterer.py
+++ src/learning/PageClusterer.py
@@ -24,7 +24,48 @@ def cluster(project_name, working_dir_str, copy_to_webapp=False):
     clusters = tf.do_truffle_shuffle(algorithm='rule_size')
     clusterCount = 1
     clusters_dir_str = os.path.join(working_dir_str, '_clusters')
-    for rule in clusters:
+    if len(clusters) > 0:
+        for rule in clusters:
+            cluster_str = 'cluster' + format(clusterCount, '03')
+            clusterCount += 1
+                    
+            page_count = 0;
+            print cluster_str + " -- " + str(len(clusters[rule]['MEMBERS']))
+            if len(clusters[rule]['MEMBERS']) > 5:
+                
+                #copy it into the local angular_flask web directory
+                markup_file = None
+                if copy_to_webapp:
+                    blank = os.path.join(web_app_projects_dir, '_blank')
+                    project_dir = os.path.join(web_app_projects_dir, project_name+"_"+cluster_str)
+                    shutil.copytree(blank, project_dir)
+                
+                    markup_file = os.path.join(project_dir, 'learning', 'markup.json')
+                    with codecs.open(markup_file, "r", "utf-8") as myfile:
+                        json_str = myfile.read().encode('utf-8')
+                    markup = json.loads(json_str)
+                
+                cluster_dir_str = os.path.join(clusters_dir_str, cluster_str)
+                if not os.path.exists(cluster_dir_str):
+                    os.makedirs(cluster_dir_str)
+                    
+                for page_id in clusters[rule]['MEMBERS']:
+                    copyfile(os.path.join(working_dir_str, page_id), os.path.join(cluster_dir_str, page_id))
+                    
+                    if copy_to_webapp:
+                        if page_count < 7:
+                            #and copy it to the web_app_dir if we have less than 5 there
+                            copyfile(os.path.join(working_dir_str, page_id), os.path.join(project_dir, page_id))
+                            markup['__URLS__'][page_id] = page_id
+                            markup[page_id] = {}
+    
+                    page_count += 1
+            
+                if copy_to_webapp:
+                    with codecs.open(markup_file, "w", "utf-8") as myfile:
+                        myfile.write(json.dumps(markup, sort_keys=True, indent=2, separators=(',', ': ')))
+                        myfile.close()
+    else:
         cluster_str = 'cluster' + format(clusterCount, '03')
         
         #copy it into the local angular_flask web directory
@@ -42,22 +83,21 @@ def cluster(project_name, working_dir_str, copy_to_webapp=False):
         clusterCount += 1
                 
         page_count = 0;
-        if len(clusters[rule]['MEMBERS']) > 5:
-            cluster_dir_str = os.path.join(clusters_dir_str, cluster_str)
-            if not os.path.exists(cluster_dir_str):
-                os.makedirs(cluster_dir_str)
+        cluster_dir_str = os.path.join(clusters_dir_str, cluster_str)
+        if not os.path.exists(cluster_dir_str):
+            os.makedirs(cluster_dir_str)
                 
-            for page_id in clusters[rule]['MEMBERS']:
-                copyfile(os.path.join(working_dir_str, page_id), os.path.join(cluster_dir_str, page_id))
-                
-                if copy_to_webapp:
-                    if page_count < 7:
-                        #and copy it to the web_app_dir if we have less than 5 there
-                        copyfile(os.path.join(working_dir_str, page_id), os.path.join(project_dir, page_id))
-                        markup['__URLS__'][page_id] = page_id
-                        markup[page_id] = {}
+        for page_id in tf.get_page_manager().getPageIds():
+            copyfile(os.path.join(working_dir_str, page_id), os.path.join(cluster_dir_str, page_id))
+            
+            if copy_to_webapp:
+                if page_count < 7:
+                    #and copy it to the web_app_dir if we have less than 5 there
+                    copyfile(os.path.join(working_dir_str, page_id), os.path.join(project_dir, page_id))
+                    markup['__URLS__'][page_id] = page_id
+                    markup[page_id] = {}
 
-                page_count += 1
+            page_count += 1
         
         if copy_to_webapp:
             with codecs.open(markup_file, "w", "utf-8") as myfile:
